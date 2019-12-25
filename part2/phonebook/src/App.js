b4295ff6c737228f14c1d716e5b1174302155d10
absurdh3ro <mmetr14@freeuni.edu.ge>
2019-12-25 18:11:37 +0400

    p2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   return (<div>filter by names:<input value={props.filterName} onChange={props.filterChanged} /></div>)
@@ -27,15 +27,20 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(res => {
+      console.log(res);
+      if (res.data) {
+        setPersons(res.data);
+      }
+    });
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
